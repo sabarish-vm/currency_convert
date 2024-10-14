d51ab95e21e6edfd8c06846190b910c5289acd2a
sabarish-vm <sabarish.science@gmail.com>
2024-10-14 21:03:45 +0200

    cleanup removed old comments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Arg, ArgAction, Command, Parser};
+use clap::{Arg, ArgAction, Command};
 use curl::easy::Easy;
 use std::collections::HashMap;
 use std::fs::{read_to_string, File};
@@ -15,18 +15,11 @@ enum Value {
 impl Value {
     fn get_string(&self) -> Option<&String> {
         if let Value::Str(s) = self {
-            Some(s) // Returning a reference to the integer
+            Some(s)
         } else {
             None
         }
     }
-    // fn get_float(&self) -> Option<&f64> {
-    //     if let Value::Float(f) = self {
-    //         Some(f) // Returning a reference to the integer
-    //     } else {
-    //         None
-    //     }
-    // }
 }
 
 fn read_file_contents(path: &PathBuf) -> Result<Vec<String>, Box<dyn std::error::Error>> {
@@ -64,15 +57,6 @@ fn downloader(path: &PathBuf) {
     let mut transfer = easy.transfer();
     transfer
         .write_function(|data| {
-            // Method 1 : More clean to read, look up curl-4.47 docs
-            // match file.write_all(data) {
-            //     Ok(_) => Ok(data.len()),
-            //     Err(_) => Err(curl::easy::WriteError::Pause),
-            // }
-            // Method 2 : Lesser number of lines and a bit tricky to understand
-            // First line below either returns an erro through unwrap()
-            // or Second line returns usize if no errors occur
-            // Thus both methods are equivalent
             file.write_all(data).unwrap();
             Ok(data.len())
         })
@@ -94,41 +78,6 @@ fn converter(amt: &f64, from: &str, to: &str, map: &HashMap<String, Value>) {
     } else {
         println!("One or both keys do not have a Float value.");
     }
-    //    //    let _mto = map.get(from) also works for some reason
-    //    let _mfrom = &map[from];
-    //    let _mto = &map[to];
-    //    let res: f64;
-    //    let _fac_to = match _mto {
-    //        Value::Float(f) => Some(f),
-    //        Value::Str(s) => None,
-    //    };
-    //    let _fac_from = match _mfrom {
-    //        Value::Float(f) => Some(f),
-    //        Value::Str(s) => None,
-    //    };
-    //    if let Some(fac_to) = _fac_to {
-    //        if let Some(fac_from) = _fac_from {
-    //            res = fac_from + fac_to
-    //        }
-    //    };
-}
-
-#[derive(Parser)]
-#[command(name = "Curconv")]
-#[command(about = "Currency converter application", long_about = None)]
-struct Cli {
-    #[arg(short, long,action = clap::ArgAction::SetTrue,group="update")]
-    update: Option<bool>,
-
-    #[arg(group = "conv")]
-    amount: f64,
-
-    #[arg(group = "conv")]
-    inpcur: String,
-
-    #[arg(group = "conv")]
-    #[arg(short, long)]
-    to: String,
 }
 
 fn main() {
@@ -160,7 +109,6 @@ fn main() {
         .arg(Arg::new("CUR1"))
         .arg(Arg::new("CUR2").short('t').long("to"))
         .get_matches();
-    // println!("A");
     let upd = matches.get_flag("update");
     if upd {
         downloader(&zip_path);
@@ -168,7 +116,6 @@ fn main() {
         println!("Update successful");
         std::process::exit(0)
     }
-    // print!("B");
     if let (Some(amount), Some(inpcur), Some(tocur)) = (
         matches.get_one::<f64>("amount"),
         matches.get_one::<String>("CUR1"),
@@ -177,16 +124,4 @@ fn main() {
         println!("Using forex rates as on {}", date);
         converter(amount, inpcur, tocur, &map)
     }
-    // let args = Cli::parse();
-    // if let Some(upd) = args.update {
-    //     println!("{}", upd);
-    //     downloader(&zip_path);
-    //     unzipper(&zip_path, &output_dir);
-    //     std::process::exit(0)
-    // }
-    // if let (amount, inpcur, tocur) = (args.amount, args.inpcur, args.to) {
-    //     converter(&(amount), &(inpcur), &(tocur), &map)
-    // } else {
-    //     println!("Either run it in update mode by using --update or -u. Or run it in conversion mode by passing args like \n\tAMT CUR1 --to CUR2")
-    // }
 }
